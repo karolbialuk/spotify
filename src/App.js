153d35219fbddac90451378c6012bec482e1ede9
John Doe <johndoe@example.com>
2023-06-26 22:39:54 +0200

    Pre Alfa Version 1.1.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {
+  LoginPage,
+  HomePage,
+  PlaylistPage,
+  FavouritePage,
+  SearchPage,
+  CategoryPage,
+  AuthorPage,
+} from './pages'
 
 import './index.scss'
 
@@ -31,6 +41,44 @@ const App = () => {
   useEffect(() => {
     setToken(localStorage.getItem('accessToken'))
   }, [window.location.hash])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={<HomePage token={localStorage.getItem('accessToken')} />}
+        />
+        <Route
+          path="/playlist/:id"
+          element={<PlaylistPage token={localStorage.getItem('accessToken')} />}
+        />
+        <Route
+          path="/album/:id"
+          element={<PlaylistPage token={localStorage.getItem('accessToken')} />}
+        />
+        <Route
+          path="/favourite"
+          element={
+            <FavouritePage token={localStorage.getItem('accessToken')} />
+          }
+        />
+        <Route
+          path="/search"
+          element={<SearchPage token={localStorage.getItem('accessToken')} />}
+        />
+        <Route
+          path="/category/:id"
+          element={<CategoryPage token={localStorage.getItem('accessToken')} />}
+        />
+        <Route
+          path="/author/:id"
+          element={<AuthorPage token={localStorage.getItem('accessToken')} />}
+        />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,55 +4,10 @@ import App from './App'
 import './global-styles/style.scss'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import {
-  LoginPage,
-  HomePage,
-  PlaylistPage,
-  FavouritePage,
-  SearchPage,
-  CategoryPage,
-  AuthorPage,
-} from './pages'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
     <App />
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/home"
-          element={<HomePage token={localStorage.getItem('accessToken')} />}
-        />
-        <Route
-          path="/playlist/:id"
-          element={<PlaylistPage token={localStorage.getItem('accessToken')} />}
-        />
-        <Route
-          path="/album/:id"
-          element={<PlaylistPage token={localStorage.getItem('accessToken')} />}
-        />
-        <Route
-          path="/favourite"
-          element={
-            <FavouritePage token={localStorage.getItem('accessToken')} />
-          }
-        />
-        <Route
-          path="/search"
-          element={<SearchPage token={localStorage.getItem('accessToken')} />}
-        />
-        <Route
-          path="/category/:id"
-          element={<CategoryPage token={localStorage.getItem('accessToken')} />}
-        />
-        <Route
-          path="/author/:id"
-          element={<AuthorPage token={localStorage.getItem('accessToken')} />}
-        />
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
   </Provider>,
 )
